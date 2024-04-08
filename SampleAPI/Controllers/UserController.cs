1113ac1d26e9667cf416704974dc943106de4cb7
unknown <sjprajapati.ele3@gmail.com>
2024-04-08 17:10:17 +0530

    first commit

---
+++ SampleAPI/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Entity;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        public UserController(ILogger<UserController> logger) { 
+        _logger=logger;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add([FromBody]User user)
+        {
+
+            if(user == null)
+            {
+                return BadRequest("Request is not valid");
+            }
+
+            if(!ModelState.IsValid) {
+                return BadRequest("Required field should not be blank");
+            }
+
+            return Ok($"user {user.Name} added successfully");
+        }
+    }
+}
